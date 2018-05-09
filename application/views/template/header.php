e908ab58864de2da75c3b883d5bc4637ce486a83
galis30 <3.galis.galis.3@gmail.com>
2018-05-09 12:35:58 +0200

    Vytvorenie Bootsrap sablony pre strukturu stranky

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,3 +2,4 @@
 <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
 <!-- Bootstrap theme -->
 <link href="<?php echo base_url();?>assets/css/bootstraptheme.min.css" rel="stylesheet">
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
